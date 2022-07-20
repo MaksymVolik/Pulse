51f9dc11009a276e645006c900efda56e27b26c9
Maksym Volik <maksymvolik@gmail.com>
2022-07-20 18:00:57 +0300

    Adde validetion forms

--- src/js/script.js
+++ src/js/script.js
@@ -18,46 +18,94 @@
 
 // ------tiny-slider-------
 const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    speed: 600,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: true,
-    navPosition: "bottom",
-    responsive: {
-        993: {
-          nav: false
-        }
-      }
-  });
+	container: '.carousel__inner',
+	items: 1,
+	speed: 600,
+	slideBy: 'page',
+	autoplay: false,
+	controls: false,
+	nav: true,
+	navPosition: "bottom",
+	responsive: {
+		993: {
+			nav: false
+		}
+	}
+});
 
-  document.querySelector('.prev').addEventListener('click', function () {
+document.querySelector('.prev').addEventListener('click', function () {
     slider.goTo('prev');
-  });
-  document.querySelector('.next').addEventListener('click', function () {
+});
+document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
-  });
+});
 
-  $(document).ready(function(){
+$(document).ready(function(){
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-      $(this)
-        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      	$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
     function toggleSlide(item) {
-      $(item).each(function(i) {
-        $(this).on('click', function(e) {
-          e.preventDefault();
-          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-      });
+      	$(item).each(function(i) {
+        	$(this).on('click', function(e) {
+			e.preventDefault();
+			$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+			$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        	})
+      	});
     };
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-  });
+    //model
+    $('[data-modal=consultation]').on('click', function() {
+      	$('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+      	$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+      	$(this).on('click', function() {
+			$('#order .modal__subheader').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+      	});
+    });
+
+	function validateForm(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				// simple rule, converted to {required:true}
+				phone: "required",
+				// compound rule
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Введіть своє ім'я",
+					minlength: jQuery.validator.format("Введіть неменше {0} символів!")
+				},
+				phone: "Введіть свій номер телефону",
+				email: {
+					required: "Введіть свою пошту",
+					email: "Ваш email адрес повинен бути в форматі name@domain.com"
+				}
+			}
+		});
+	};
+
+	validateForm('#consultation-form');
+	validateForm('#consultation form');
+	validateForm('#order form');
+});
